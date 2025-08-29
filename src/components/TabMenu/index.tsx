2d5ee3517a3a7096f0a6efb4422d9006c585c8bf
Dionei Bianchati <dion.biancha@gmail.com>
2025-08-29 15:20:01 -0300

    fix: remoção de erro

--- src/components/TabMenu/index.tsx
+++ src/components/TabMenu/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import profileImg from "../../assets/profile.jpeg";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
